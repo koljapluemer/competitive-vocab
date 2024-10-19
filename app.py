d42af8b765e833e937f9b8fc59b66cc23588a3ac
blackbrokkoli <kastomat@gmail.com>
2024-10-19 10:30:06 +0200

    build extremely basic vocab loader

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import csv
+import random
+import os
+
 
 app = Flask(__name__)
-
+static_folder_path = os.path.join(app.root_path, 'static')
+vocab_csv_name = 'vocab_marta_24_10_1.csv'
 
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+def main_test():
+    # load static file vocab_marta_24_10_1.csv 
+    # ONLY rows where the first and 2nd column are non-empty are valid vocab
+    # pick one word combination randomly and serve to template
+    vocab = []
+    csv_path = os.path.join(static_folder_path, vocab_csv_name)
+    with open(csv_path, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] and row[1]:
+                vocab.append(row)
+
+    picked_vocab = random.choice(vocab)
+
+    return render_template('game_from_four.html', vocab=picked_vocab)
