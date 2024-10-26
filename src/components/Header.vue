b303e499d05861f78ce74cf3b05c3c802efab00b
blackbrokkoli <kastomat@gmail.com>
2024-10-26 19:12:50 +0200

    add progress bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,19 @@
   <header class="border-b-2 mb-10 pb-2" v-if="userStore.user">
     <nav class="navbar bg-base-100 w-full flex flex-row justify-between">
       <div class="font-bold">{{ userStore.user }}: {{ userStore.userScore }}</div>
-      <div class="font-bold">{{ userStore.opponent }}: {{ userStore.opponentScore }}</div>
+
+      <div class="flex flex-col gap-1 w-full">
+        <progress
+          class="w-full"
+          :value="userStore.userScore"
+          :max="userStore.userScore + userStore.opponentScore"
+        ></progress>
+        <small v-if="userStore.userScore < userStore.opponentScore">You're currently behind</small>
+        <small v-else-if="userStore.userScore > userStore.opponentScore">You're currently ahead</small>
+      </div>
+      <div class="font-bold text-right">
+        {{ userStore.opponent }}: {{ userStore.opponentScore }}
+      </div>
     </nav>
 
     <nav class="flex flew-row gap-2 justify-center">
