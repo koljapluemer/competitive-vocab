c17d366d2d81e89865dcafd0f691d061e5091645
blackbrokkoli <kastomat@gmail.com>
2024-10-28 19:36:31 +0100

    add several iprovements and fixes

--- src/components/Games/SelectFromFourTarget.vue
+++ src/components/Games/SelectFromFourTarget.vue
@@ -4,6 +4,7 @@
       {{ currentQuestion.word_native || "Loading..." }}
     </h2>
     <div class="flex flex-col gap-2" v-if="answerOptions.length">
+      <!-- TODO: shuffle this but not broken -->
       <button
         v-for="(option, index) in answerOptions"
         :key="index"
@@ -31,12 +32,12 @@ const answerOptions = ref([]);
 const numberOfWrongClicks = ref(0);
 
 // Fetch the next due card when the component mounts
-onMounted( () => {
+onMounted(() => {
   loadNewQuestion();
 });
 
 // Fetch and set the next due card as the current question
-const loadNewQuestion =  () => {
+const loadNewQuestion = () => {
   console.log("Loading new question");
   const wordsForPuzzle = vocabStore.getWords(4); // returns 4!!! words, 1 to be used as the correct answer
   console.log("words for puzzle", wordsForPuzzle);
@@ -46,12 +47,12 @@ const loadNewQuestion =  () => {
   for (const word of wordsForPuzzle) {
     answerOptions.value.push(word.word_target);
   }
+  answerOptions.value = answerOptions.value.sort(() => 0.5 - Math.random());
 };
 
 // Handle answer selection
-const selectAnswer =  (selected) => {
+const selectAnswer = (selected) => {
   if (selected === currentQuestion.value.word_target) {
-
     const newScore = userStore.userScore + 3;
     userStore.updateScore(newScore); // Update score in Supabase and userStore
 
