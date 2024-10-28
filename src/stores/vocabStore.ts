9fceda72373ea42ec4f000302eca470df15220bf
blackbrokkoli <kastomat@gmail.com>
2024-10-28 15:29:55 +0100

    correctly save SR state, and log SR reps

--- src/stores/vocabStore.ts
+++ src/stores/vocabStore.ts
@@ -1,7 +1,17 @@
 import { defineStore } from "pinia";
 
 import { supabase } from "../supabase";
-import { get } from "http";
+import {
+  createEmptyCard,
+  formatDate,
+  fsrs,
+  generatorParameters,
+  Rating,
+  Grades,
+  FSRS,
+  FSRSParameters,
+  RecordLog,
+} from "ts-fsrs";
 
 // first, create a simple store, ignoring fsrs for now
 export const useVocabStore = defineStore("vocabStore", {
@@ -20,6 +30,12 @@ export const useVocabStore = defineStore("vocabStore", {
       }
       this.words = data || [];
       console.log("Vocabulary loaded:", this.words);
+
+      const localLearningData = localStorage.getItem("localLearningData");
+      if (localLearningData) {
+        this.localLearningData = JSON.parse(localLearningData);
+        console.log("Local learning data loaded:", this.localLearningData);
+      }
     },
     async getOneWord() {
       if (this.words.length === 0) {
@@ -39,7 +55,63 @@ export const useVocabStore = defineStore("vocabStore", {
       rating: number,
       max_rating: number
     ) {
-      return;
+      // if word is not saved in localstorage, ignore actual rating and just save it
+      if (!this.localLearningData[wordNative]) {
+        console.info("New card registered: ", wordNative);
+        this.createFSRSCard(wordNative);
+        return;
+      } else {
+        const params: FSRSParameters = generatorParameters({
+          maximum_interval: 1000,
+        });
+        const f: FSRS = new FSRS(params);
+
+        console.info("Previously registered card rated: ", rating);
+        // map the rating from min 0-max-rating to 0-3
+        const mappedRating = (3 * rating) / max_rating;
+        // round down
+        const mappedRatingRounded = Math.floor(mappedRating);
+        console.log("mapped rating: ", mappedRatingRounded);
+        // use ts-fsrs grades:
+        // 0: Again, 1: Hard, 2: Good, 3: Easy
+        let card = this.localLearningData[wordNative];
+        const schedulingCards: RecordLog = f.repeat(card, new Date());
+        switch (mappedRatingRounded) {
+          case 0:
+            card = schedulingCards[Rating.Again].card;
+            break;
+          case 1:
+            card = schedulingCards[Rating.Hard].card;
+            break;
+          case 2:
+            card = schedulingCards[Rating.Good].card;
+            break;
+          case 3:
+            card = schedulingCards[Rating.Easy].card;
+            break;
+          default:
+            console.error("Rating out of range: ", mappedRatingRounded);
+            return;
+        }
+
+        // update localstorage/state
+        this.localLearningData[wordNative] = card;
+        localStorage.setItem(
+          "localLearningData",
+          JSON.stringify(this.localLearningData)
+        );
+      }
+    },
+
+    // this is called when a word is not yet rated
+    // and not yet stored in localstorage
+    createFSRSCard(wordNative: string) {
+      const card = createEmptyCard();
+      this.localLearningData[wordNative] = card;
+      localStorage.setItem(
+        "localLearningData",
+        JSON.stringify(this.localLearningData)
+      );
     },
   },
 });
